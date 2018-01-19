cf28d677821527a6ba4439ec2eb597ce4c7f469c
Gyeong-Sik Park <tpnet3@gmail.com>
2018-01-20 00:07:14 +0900

    edit

--- example/app.js
+++ example/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello, bouncecode!'));
+app.get('/', (req, res) => res.send('Hello, my app!'));
 app.listen(4000, () => console.log('listening on port 4000!'));
